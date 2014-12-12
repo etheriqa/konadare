6785c9d53f9df1f2e5cb647a285a0e5113013425
Kaede Takamori <etheriqa@gmail.com>
2014-12-13 02:17:37 +0900

    Add factorize()

---
+++ src/factorize.cc
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <map>
+
+using factor_t = std::map<long long, int>;
+
+factor_t factorize(long long x)
+{
+  factor_t factor;
+  if (x <= 1) return factor;
+  for (int i = 2; x > 1 && i <= ceil(sqrt(x)); i++) {
+    while (x % i == 0) {
+      x /= i;
+      factor[i] += 1;
+    }
+  }
+  if (x > 1) factor[x] += 1;
+  return factor;
+}
