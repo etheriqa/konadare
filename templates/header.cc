9d85aa0be159c7408bcd2e276026d0f7219b09cb
Kaede Takamori <etheriqa@gmail.com>
2014-12-26 22:42:55 +0900

    Add macros

--- templates/header.cc
+++ templates/header.cc
@@ -7,6 +7,8 @@
 #define rep(i,n) for (int i = 0; i < (n); i++)
 #define asc(c) (c).begin(), (c).end()
 #define desc(c) (c).rbegin(), (c).rend()
+#define ascend(l,r) l = max(l, r)
+#define descend(l,r) l = min(l, r)
 #define mp(...) make_pair(__VA_ARGS__)
 #define mt(...) make_tuple(__VA_ARGS__)
 
