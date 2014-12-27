76592c043d9b5d1e176be3580d76f933e56dd0e8
Kaede Takamori <etheriqa@gmail.com>
2014-12-27 20:13:33 +0900

    Add extendEuclidean<Arithmetic>()

---
+++ src/extend_euclidean.cc
@@ -0,0 +1,19 @@
+/**
+ * Extend Euclidean algorithm
+ */
+
+#include <utility>
+
+template <class Arithmetic>
+std::pair<Arithmetic, Arithmetic> extendEuclidean(Arithmetic a, Arithmetic b)
+{
+  if (a < b) {
+    auto yx = extend_euclidean(b, a);
+    return std::make_pair(yx.second, yx.first);
+  }
+  if (b == 0) {
+    return std::make_pair(1, 0);
+  }
+  auto xy = extend_euclidean(b, a % b);
+  return std::make_pair(xy.second, xy.first - a / b * xy.second);
+}
