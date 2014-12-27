7c8b8059d7cf1ea9eceeccee6ac23bb016eeb07e
Kaede Takamori <etheriqa@gmail.com>
2014-12-28 01:35:17 +0900

    Rename the method name

--- src/extended_euclidean.cc
+++ src/extended_euclidean.cc
@@ -1,19 +1,19 @@
 /**
- * Extend Euclidean algorithm
+ * Extended Euclidean algorithm
  */
 
 #include <utility>
 
 template <class Arithmetic>
-std::pair<Arithmetic, Arithmetic> extend_euclidean(Arithmetic a, Arithmetic b)
+std::pair<Arithmetic, Arithmetic> extended_euclidean(Arithmetic a, Arithmetic b)
 {
   if (a < b) {
-    auto yx = extend_euclidean(b, a);
+    auto yx = extended_euclidean(b, a);
     return std::make_pair(yx.second, yx.first);
   }
   if (b == 0) {
     return std::make_pair(1, 0);
   }
-  auto xy = extend_euclidean(b, a % b);
+  auto xy = extended_euclidean(b, a % b);
   return std::make_pair(xy.second, xy.first - a / b * xy.second);
 }
--- src/extended_euclidean_test.cc
+++ src/extended_euclidean_test.cc
@@ -3,29 +3,29 @@
 #include <utility>
 #include <vector>
 #include "gtest/gtest.h"
-#include "extend_euclidean.cc"
+#include "extended_euclidean.cc"
 
-TEST(ExtendEuclidean, Small)
+TEST(ExtendedEuclidean, Small)
 {
   std::pair<int, int> xy;
 
-  xy = extend_euclidean(1, 0);
+  xy = extended_euclidean(1, 0);
   EXPECT_EQ(1, 1 * xy.first + 0 * xy.second);
 
-  xy = extend_euclidean(1, 1);
+  xy = extended_euclidean(1, 1);
   EXPECT_EQ(1, 1 * xy.first + 1 * xy.second);
 
-  xy = extend_euclidean(1, 2);
+  xy = extended_euclidean(1, 2);
   EXPECT_EQ(1, 1 * xy.first + 2 * xy.second);
 
-  xy = extend_euclidean(2, 3);
+  xy = extended_euclidean(2, 3);
   EXPECT_EQ(1, 2 * xy.first + 3 * xy.second);
 
-  xy = extend_euclidean(12, 18);
+  xy = extended_euclidean(12, 18);
   EXPECT_EQ(6, 12 * xy.first + 18 * xy.second);
 }
 
-TEST(ExtendEuclidean, Large)
+TEST(ExtendedEuclidean, Large)
 {
   const size_t n_cases = 1e6;
   const long long supremum = 1e6;
@@ -35,7 +35,7 @@ TEST(ExtendEuclidean, Large)
   for (size_t i = 0; i < n_cases; i++) {
     long long a = engine() % supremum + 1;
     long long b = engine() % supremum + 1;
-    auto xy = extend_euclidean(a, b);
+    auto xy = extended_euclidean(a, b);
     EXPECT_EQ(std::__gcd(a, b), a * xy.first + b * xy.second);
   }
 }
