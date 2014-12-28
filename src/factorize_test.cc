e60dd0b34cc881af718ac88269ae46cccd94be28
Kaede Takamori <etheriqa@gmail.com>
2014-12-28 16:56:22 +0900

    Refactor the tests

--- src/factorize_test.cc
+++ src/factorize_test.cc
@@ -1,4 +1,5 @@
-#include <map>
+#include <limits>
+#include <random>
 #include "gtest/gtest.h"
 #include "factorize.cc"
 
@@ -7,42 +8,43 @@ TEST(Factorize, Small)
   EXPECT_TRUE(factorize(-1).empty());
   EXPECT_TRUE(factorize(0).empty());
   EXPECT_TRUE(factorize(1).empty());
-
-  std::map<long long, std::map<long long, int>> fixtures({
-    {2, {{2, 1}}},
-    {3, {{3, 1}}},
-    {4, {{2, 2}}},
-    {5, {{5, 1}}},
-    {6, {{2, 1}, {3, 1}}},
-    {7, {{7, 1}}},
-    {8, {{2, 3}}},
-    {9, {{3, 2}}},
-    {10, {{2, 1}, {5, 1}}},
-    {11, {{11, 1}}},
-    {12, {{2, 2}, {3, 1}}},
-    {13, {{13, 1}}},
-    {14, {{2, 1}, {7, 1}}},
-    {15, {{3, 1}, {5, 1}}},
-    {16, {{2, 4}}},
-    {17, {{17, 1}}},
-    {18, {{2, 1}, {3, 2}}},
-    {19, {{19, 1}}},
-    {20, {{2, 2}, {5, 1}}}
-  });
-
-  for (auto f : fixtures) {
-    EXPECT_EQ(f.second, factorize(f.first));
-  }
+  EXPECT_EQ(factor_t({{2, 1}}), factorize(2));
+  EXPECT_EQ(factor_t({{3, 1}}), factorize(3));
+  EXPECT_EQ(factor_t({{2, 2}}), factorize(4));
+  EXPECT_EQ(factor_t({{5, 1}}), factorize(5));
+  EXPECT_EQ(factor_t({{2, 1}, {3, 1}}), factorize(6));
+  EXPECT_EQ(factor_t({{7, 1}}), factorize(7));
+  EXPECT_EQ(factor_t({{2, 3}}), factorize(8));
+  EXPECT_EQ(factor_t({{3, 2}}), factorize(9));
+  EXPECT_EQ(factor_t({{2, 1}, {5, 1}}), factorize(10));
+  EXPECT_EQ(factor_t({{11, 1}}), factorize(11));
+  EXPECT_EQ(factor_t({{2, 2}, {3, 1}}), factorize(12));
+  EXPECT_EQ(factor_t({{13, 1}}), factorize(13));
+  EXPECT_EQ(factor_t({{2, 1}, {7, 1}}), factorize(14));
+  EXPECT_EQ(factor_t({{3, 1}, {5, 1}}), factorize(15));
+  EXPECT_EQ(factor_t({{2, 4}}), factorize(16));
+  EXPECT_EQ(factor_t({{17, 1}}), factorize(17));
+  EXPECT_EQ(factor_t({{2, 1}, {3, 2}}), factorize(18));
+  EXPECT_EQ(factor_t({{19, 1}}), factorize(19));
+  EXPECT_EQ(factor_t({{2, 2}, {5, 1}}), factorize(20));
+  EXPECT_EQ(factor_t({{2, 8}, {3, 4}, {5, 2}, {7, 1}}), factorize(3628800));
 }
 
 TEST(Factorize, Large)
 {
-  std::map<long long, std::map<long long, int>> fixtures({
-    {3628800, {{2, 8}, {3, 4}, {5, 2}, {7, 1}}},
-    {2432902008176640000ll, {{2, 18}, {3, 8}, {5, 4}, {7, 2}, {11, 1}, {13, 1}, {17, 1}, {19, 1}}}
-  });
+  const size_t query = 1e4;
+  std::random_device seed;
+  std::default_random_engine engine(seed());
 
-  for (auto f : fixtures) {
-    EXPECT_EQ(f.second, factorize(f.first));
+  for (size_t i = 0; i < query; i++) {
+    int n = engine() % std::numeric_limits<int>::max() + 1;
+    factor_t f = factorize(n);
+    int product = 1;
+    for (auto p : f) {
+      for (size_t j = 0; j < p.second; j++) {
+        product *= p.first;
+      }
+    }
+    ASSERT_EQ(n, product);
   }
 }
