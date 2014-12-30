15f4c917013732015767c2a0243026db680f2eb7
Kaede Takamori <etheriqa@gmail.com>
2014-12-31 04:49:10 +0900

    Add UnionFind<Key>::getFamily()

--- src/union_find.cc
+++ src/union_find.cc
@@ -4,15 +4,18 @@
 
 #include <map>
 #include <utility>
+#include <vector>
 
 template <class Key>
 class UnionFind
 {
-  std::map<Key, std::pair<Key, size_t>> uf;
+  using size_type = std::size_t;
+
+  std::map<Key, std::pair<Key, size_type>> uf_;
 public:
   void insert(const Key& x)
   {
-    uf[x] = std::make_pair(x, 0);
+    uf_[x] = std::make_pair(x, 0);
   }
   void merge(const Key& x, const Key& y)
   {
@@ -20,8 +23,8 @@ public:
     Key y_root = find(y);
     if (x == y) { return; }
 
-    auto& px = uf[x_root];
-    auto& py = uf[y_root];
+    auto& px = uf_[x_root];
+    auto& py = uf_[y_root];
     if (px.second < py.second) {
       px.first = y_root;
     } else if (px.second > py.second) {
@@ -33,7 +36,7 @@ public:
   }
   Key find(const Key& x)
   {
-    auto& px = uf[x];
+    auto& px = uf_[x];
     if (x == px.first) {
       return x;
     } else {
@@ -43,5 +46,13 @@ public:
   bool isSame(const Key& x, const Key& y)
   {
     return find(x) == find(y);
+  }
+  std::vector<std::vector<Key>> getFamily()
+  {
+    std::map<Key, std::vector<Key>> family;
+    for (auto& p : uf_) {
+      family[find(p.first)].push_back(p.first);
+    }
+    return family;
   }
 };
