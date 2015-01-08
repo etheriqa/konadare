54638d02967a43ad08e30ff76016ca17e9eaa107
Kaede Takamori <etheriqa@gmail.com>
2015-01-09 00:30:25 +0900

    Add tests

--- src/dijkstra_test.cc
+++ src/dijkstra_test.cc
@@ -2,7 +2,30 @@
 #include "gtest/gtest.h"
 #include "dijkstra.cc"
 
-TEST(Dijkstra, Small)
+TEST(Dijkstra, Small1)
+{
+  auto pred = dijkstra<int, int>(
+    0,
+    [](int){ return std::vector<int>(0); },
+    [](int, int){ return 1; }
+  );
+  ASSERT_EQ(1, pred.size());
+  ASSERT_EQ(0, pred.at(0));
+}
+
+TEST(Dijkstra, Small2)
+{
+  auto pred = dijkstra<int, int>(
+    0,
+    [](int){ return std::vector<int>({0, 1}); },
+    [](int, int){ return 1; }
+  );
+  ASSERT_EQ(2, pred.size());
+  ASSERT_EQ(0, pred.at(0));
+  ASSERT_EQ(0, pred.at(1));
+}
+
+TEST(Dijkstra, Small3)
 {
   std::vector<std::vector<int>> weight = {
     {    0,     3,     6,    12 ,10000, 10000},
@@ -34,8 +57,8 @@ TEST(Dijkstra, Small)
   EXPECT_EQ(0, pred.count(4));
   EXPECT_EQ(0, pred.count(5));
 
-  EXPECT_EQ(0, pred[0]);
-  EXPECT_EQ(0, pred[1]);
-  EXPECT_EQ(0, pred[2]);
-  EXPECT_EQ(1, pred[3]);
+  EXPECT_EQ(0, pred.at(0));
+  EXPECT_EQ(0, pred.at(1));
+  EXPECT_EQ(0, pred.at(2));
+  EXPECT_EQ(1, pred.at(3));
 }
